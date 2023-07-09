5a2b8b49ba12f013b2f664a7212253bf91d1e4d5
Yeong joon <bluehome8626@naver.com>
2023-07-10 00:03:44 +0900

    feat : 부스 점검

--- dsm-payments-admin/src/apis/index.ts
+++ dsm-payments-admin/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://api.dsm-pay.com";
+export const BASE_URL = "http://15.165.47.216:8080";
 
 export const LOGIN_URI = "/admin/auth";
 export const USER_PAY_URI = "/admin/pay/user";
